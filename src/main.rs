1a3314be34a9025c2e36254a30967880ff617076
WinLinux1028 <reimussr@protonmail.ch>
2022-05-25 01:33:41 +0900

    LINEの最大文字数を出力するように変更

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ use std::fs::File;
 use std::io::Write;
 
 fn main() {
-    let mut a: Vec<u8> = "𰻞".as_bytes().to_vec();
-    for _ in 0..1998 {
-        a.push(205);
-        a.push(OsRng.gen_range(128..=175));
+    let mut a = Vec::new();
+    for _ in 0..10 {
+        a.extend_from_slice("𰻞".as_bytes());
+        for _ in 0..999 {
+            a.push(205);
+            a.push(OsRng.gen_range(128..=175));
+        }
     }
     // 正当なUTF-8であることを確認
     let a = String::from_utf8(a).unwrap();
