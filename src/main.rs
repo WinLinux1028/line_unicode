4db4639d90ac7b9e30c5aa1aaa97f9678743eb05
WinLinux1028 <reimussr@protonmail.ch>
2022-05-25 14:49:02 +0900

    aaa

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,19 @@ use std::fs::File;
 use std::io::Write;
 
 fn main() {
-    let mut v: Vec<u32> = (0x0300..=0x036F).collect();
+    let mut v: Vec<u32> = (0x0300..=0x0357).collect();
+    v.append(&mut (0x0359..=0x036F).collect());
     v.append(&mut (0x1DC0..=0x1DF5).collect());
     v.append(&mut (0x1DFC..=0x1DFF).collect());
     v.append(&mut (0x1AB0..=0x1ABE).collect());
     v.append(&mut (0xFE20..=0xFE2D).collect());
-    v.append(&mut (0x20DB..=0x20DE).collect());
-    v.push(0x20E8);
-    v.push(0x20F0);
+    v.append(&mut (0x20D0..=0x20DE).collect());
+    v.append(&mut (0x20E1..=0x20E2).collect());
+    v.append(&mut (0x20E5..=0x20F0).collect());
     let mut v: Vec<char> = unsafe { std::mem::transmute(v) };
 
     let mut a = String::new();
-    while 24449 >= a.len() {
+    while 25432 >= a.len() {
         v.shuffle(&mut OsRng);
         a.push_str("🇷🇺");
         for i in &v {
